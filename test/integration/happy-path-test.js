db90761026f964cdc53778dabce48c9fa7977919
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-01-09 16:54:44 -0800

    build: cleanups

--- test/integration/happy-path-test.js
+++ test/integration/happy-path-test.js
@@ -1,59 +1,59 @@
-const { test } = require('tap')
+const { test } = require("tap");
 
-const Octokit = require('@octokit/rest')
-  .plugin(require('../..'))
+const Octokit = require("@octokit/rest").plugin(require("../.."));
 
-test('happy path', async t => {
-  const fixtures = require('./fixtures/happy-path')
-  const octokit = new Octokit()
+test("happy path", async (t) => {
+  const fixtures = require("./fixtures/happy-path");
+  const octokit = new Octokit();
 
-  octokit.hook.wrap('request', (_, options) => {
-    const currentFixtures = fixtures.shift()
-    const { baseUrl, method, url, request, headers, mediaType, ...params } = options
+  octokit.hook.wrap("request", (_, options) => {
+    const currentFixtures = fixtures.shift();
+    const { baseUrl, method, url, request, headers, mediaType, ...params } =
+      options;
 
-    t.equal(currentFixtures.request.method, options.method)
-    t.equal(currentFixtures.request.url, options.url)
+    t.equal(currentFixtures.request.method, options.method);
+    t.equal(currentFixtures.request.url, options.url);
 
-    Object.keys(params).forEach(paramName => {
-      t.deepEqual(currentFixtures.request[paramName], params[paramName])
-    })
-    return currentFixtures.response
-  })
+    Object.keys(params).forEach((paramName) => {
+      t.same(currentFixtures.request[paramName], params[paramName]);
+    });
+    return currentFixtures.response;
+  });
 
-  const owner = 'gr2m'
-  const repo = 'octokit-pinned-issues-fixtures'
+  const owner = "gr2m";
+  const repo = "octokit-pinned-issues-fixtures";
 
   // returns empty array
   await octokit.getPinnedIssues({
     owner,
-    repo
-  })
+    repo,
+  });
 
   // pin first issue
   await octokit.pinIssue({
     owner,
     repo,
-    number: 1
-  })
+    number: 1,
+  });
 
   // returns array with pinned issue
   await octokit.getPinnedIssues({
     owner,
-    repo
-  })
+    repo,
+  });
 
   // unpin issue
   await octokit.unpinIssue({
     owner,
     repo,
-    number: 1
-  })
+    number: 1,
+  });
 
   // returns empty array
   await octokit.getPinnedIssues({
     owner,
-    repo
-  })
+    repo,
+  });
 
-  t.equal(fixtures.length, 0)
-})
+  t.equal(fixtures.length, 0);
+});
--- test/unit/restify-issue-node-test.js
+++ test/unit/restify-issue-node-test.js
@@ -1,72 +1,73 @@
-const { test } = require('tap')
+const { test } = require("tap");
 
-const restifyIssueNode = require('../../lib/restify-issue-node')
+const restifyIssueNode = require("../../lib/restify-issue-node");
 
-test('restifyIssueNode', t => {
+test("restifyIssueNode", (t) => {
   const issue = restifyIssueNode({
-    id: 'MDU6SXNzdWUzOTE0OTI4MTQ=',
+    id: "MDU6SXNzdWUzOTE0OTI4MTQ=",
     number: 71,
-    state: 'OPEN',
-    title: 'test 2',
-    body: '',
+    state: "OPEN",
+    title: "test 2",
+    body: "",
     labels: {
       nodes: [
         {
-          id: 'MDU6TGFiZWw3MTkyMjczOTg=',
-          name: 'foo',
+          id: "MDU6TGFiZWw3MTkyMjczOTg=",
+          name: "foo",
           description: null,
-          color: 'ededed'
-        }
-      ]
+          color: "ededed",
+        },
+      ],
     },
     assignees: {
       nodes: [
         {
-          login: 'gr2m',
-          id: 'MDQ6VXNlcjM5OTky',
-          avatarUrl: 'https://avatars3.githubusercontent.com/u/39992?v=4'
-        }
-      ]
+          login: "gr2m",
+          id: "MDQ6VXNlcjM5OTky",
+          avatarUrl: "https://avatars3.githubusercontent.com/u/39992?v=4",
+        },
+      ],
     },
     milestone: {
-      id: 'MDk6TWlsZXN0b25lMzkwNDcwMw==',
-      state: 'OPEN',
-      title: 'Funk',
-      description: 'Get Funky!'
+      id: "MDk6TWlsZXN0b25lMzkwNDcwMw==",
+      state: "OPEN",
+      title: "Funk",
+      description: "Get Funky!",
     },
     locked: false,
-    activeLockReason: null
-  })
+    activeLockReason: null,
+  });
 
-  t.deepEqual(issue, {
-    id: 'MDU6SXNzdWUzOTE0OTI4MTQ=',
+  t.same(issue, {
+    id: "MDU6SXNzdWUzOTE0OTI4MTQ=",
     number: 71,
-    state: 'OPEN',
-    title: 'test 2',
-    body: '',
+    state: "OPEN",
+    title: "test 2",
+    body: "",
     locked: false,
     active_lock_reason: null,
     milestone: {
-      id: 'MDk6TWlsZXN0b25lMzkwNDcwMw==',
-      state: 'OPEN',
-      title: 'Funk',
-      description: 'Get Funky!' },
+      id: "MDk6TWlsZXN0b25lMzkwNDcwMw==",
+      state: "OPEN",
+      title: "Funk",
+      description: "Get Funky!",
+    },
     labels: [
       {
-        id: 'MDU6TGFiZWw3MTkyMjczOTg=',
-        name: 'foo',
+        id: "MDU6TGFiZWw3MTkyMjczOTg=",
+        name: "foo",
         description: null,
-        color: 'ededed'
-      }
+        color: "ededed",
+      },
     ],
     assignees: [
       {
-        login: 'gr2m',
-        id: 'MDQ6VXNlcjM5OTky',
-        avatar_url: 'https://avatars3.githubusercontent.com/u/39992?v=4'
-      }
-    ]
-  })
+        login: "gr2m",
+        id: "MDQ6VXNlcjM5OTky",
+        avatar_url: "https://avatars3.githubusercontent.com/u/39992?v=4",
+      },
+    ],
+  });
 
-  t.end()
-})
+  t.end();
+});
